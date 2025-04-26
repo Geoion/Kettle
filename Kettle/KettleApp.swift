8db6f45e7c7e8dd8eae66dca6487abeda3ff2ba4
Eski Yin <Eski.Yin@Gmail.com>
2025-04-27 02:56:11 +0800

    Initial commit

---
+++ Kettle/KettleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KettleApp.swift
+//  Kettle
+//
+//  Created by Eski Yin on 2025/4/27.
+//
+
+import SwiftUI
+
+@main
+struct KettleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KettleTests/KettleTests.swift
@@ -0,0 +1,17 @@
+//
+//  KettleTests.swift
+//  KettleTests
+//
+//  Created by Eski Yin on 2025/4/27.
+//
+
+import Testing
+@testable import Kettle
+
+struct KettleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
